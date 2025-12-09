a64d566ab36b7a004dbe1a740defb0a19d7511d4
billotiende-droid <bill.otiende@student.moringaschool.com>
2025-12-09 10:03:18 +0300

    app-component-structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,14 @@
 import React from  'react';
-import {Apple, Search, User, ShoppingCart} from 'lucide-react';
+import Header from './assets/Header.jsx';
 
 
 function App() {
 
-
-
-
-
-
-
-
-
  return (
     // Main container for the entire app
-    <div className="bg-purple-400 min-h-screen">
-      
-      {/* Header section */}
-      <header className="bg-gray-200 p-6">
-        <div className="flex justify-between items-centre max-w-9xl mx-auto ">
-          {/* App Title */}
-          <div className="flex gap-1">
-            <Apple className="w-8 h-8 text-black" />
-            <h1 className="font-semibold text-2xl tracking-wide text-black ">
-              GADGET STORE KE </h1>
-          </div>
-          
-          {/* Navigation Links */}
-          <nav className="hidden md:flex space-x-10 mask-b-from-neutral-100 font-semibold">
-            <a href="#home" className="hover:text-blue-300">Store</a>
-            <a href="#about" className="hover:text-blue-300">Featured</a>
-            <a href="#resources" className="hover:text-blue-300">Accessories</a>
-             <a href="#resources" className="hover:text-blue-300">Experience</a>
-              <a href="#resources" className="hover:text-blue-300">Support</a>
-          </nav>
-
-          {/* search User Cart Icons */}
-          <div className="flex items-center gap-6" >
-            <Search className="cursor-pointer hover:textamber-300" />
-            <User className="cursor-pointer hover:text-amber-300" />
-
-            <button className="flex items-center gap-2 bg-amber-300 text-black font-semibold px-4 py-2 rounded-lg hover:bg-amber-400 transition ">
-              <ShoppingCart className="w-5 h-5" />
-              <span>Cart</span>
-            </button>
-            
-
-          </div>
-          
-        </div>
-      </header>
-
+    <div className="min-h-screen bg-gradient-to-br: from-blue-900 via-indigo-800 to-amber-700">
+      <Header />
+     
       {/* Main content area */}
       <main className="flex flex-col md:flex-row gap-4 p-6">
         
