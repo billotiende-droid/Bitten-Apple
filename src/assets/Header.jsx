38a17adbd6a564fda56774b258aa59fd7add55d1
billotiende-droid <bill.otiende@student.moringaschool.com>
2025-12-09 10:04:36 +0300

    header-component-structure-styling

---
+++ src/assets/Header.jsx
@@ -0,0 +1,39 @@
+import { Apple, Search, User, ShoppingCart } from "lucide-react";
+import React from "react";
+
+function Header() {
+  return (
+    <header className="bg-gray-200 p-6 border-b border-gray-300 rounded-2xl">
+      <div className="flex items-center max-w-7xl mx-auto w-full ml-7">
+        {/* App Title */}
+        <div className="flex items- centre gap-2">
+          <Apple className="w-8 h-8 text-black" />
+          <h1 className="font-semibold text-2xl tracking-wide text-black">
+            GADGET STORE KE
+          </h1>
+        </div>
+
+        {/* Navigation Links */}
+        <nav className=" md:flex flex-1 justify-center space-x-10 font-semibold mask-b-from-neutral-100 font-sans">
+          <a href="#home" className="hover:text-blue-300">Store</a>
+          <a href="#about" className="hover:text-blue-300">Featured</a>
+          <a href="#resources" className="hover:text-amber-500">Accessories</a>
+          <a href="#resources" className="hover:text-gray-700">Experience</a>
+          <a href="#resources" className="hover:text-blue-300">Support</a>
+        </nav>
+
+        {/* Search, User, Cart */}
+        <div className="flex items-center gap-6 ">
+          <Search className="cursor-pointer hover:text-amber-300" />
+          <User className="cursor-pointer hover:text-amber-300" />
+          <button className="flex items-center gap-2 bg-amber-300 text-black font-semibold px-4 py-2 rounded-lg hover:bg-amber-400 transition">
+            <ShoppingCart className="w-5 h-5" />
+            <span>Cart</span>
+          </button>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
