88ddc63e59a6b248cb374584aa126d0c2a013595
billotiende-droid <bill.otiende@student.moringaschool.com>
2025-12-09 10:59:27 +0300

    font-hover-colors

--- src/assets/Header.jsx
+++ src/assets/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 function Header() {
   return (
-    <header className="bg-gray-200 p-6 border-b border-gray-300 rounded-2xl">
+    <header className="sticky top-0 bg-gray-300 p-6 border-b border-gray-300 rounded-xl max-w-full">
       <div className="flex items-center max-w-7xl mx-auto w-full ml-7">
         {/* App Title */}
         <div className="flex items- centre gap-2">
@@ -14,18 +14,18 @@ function Header() {
         </div>
 
         {/* Navigation Links */}
-        <nav className=" md:flex flex-1 justify-center space-x-10 font-semibold mask-b-from-neutral-100 font-sans">
-          <a href="#home" className="hover:text-blue-300">Store</a>
-          <a href="#about" className="hover:text-blue-300">Featured</a>
-          <a href="#resources" className="hover:text-amber-500">Accessories</a>
-          <a href="#resources" className="hover:text-gray-700">Experience</a>
-          <a href="#resources" className="hover:text-blue-300">Support</a>
+        <nav className=" md:flex flex-1 justify-center space-x-10 font-semibold font-sans">
+          <a href="#home" className="hover:text-indigo-500">Store</a>
+          <a href="#about" className="hover:text-indigo-500">Featured</a>
+          <a href="#resources" className="hover:text-indigo-500">Accessories</a>
+          <a href="#resources" className="hover:text-indigo-500">Experience</a>
+          <a href="#resources" className="hover:text-cyan-500">Support</a>
         </nav>
 
         {/* Search, User, Cart */}
         <div className="flex items-center gap-6 ">
-          <Search className="cursor-pointer hover:text-amber-300" />
-          <User className="cursor-pointer hover:text-amber-300" />
+          <Search className="cursor-pointer hover:text-cyan-500" />
+          <User className="cursor-pointer hover:text-cyan-500" />
           <button className="flex items-center gap-2 bg-amber-300 text-black font-semibold px-4 py-2 rounded-lg hover:bg-amber-400 transition">
             <ShoppingCart className="w-5 h-5" />
             <span>Cart</span>
