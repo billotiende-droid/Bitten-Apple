ad72afef16beb6bf88300bdcdbab3242516bcb73
Onyango <allanbill34@gmail.com>
2026-07-13 12:18:12 +0300

    feat(auth): implement NextAuth configuration with CredentialsProvider

---
+++ front-end/bitten-apple/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,27 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from  "next-auth/providers/credentials"
+
+
+export const authOptions = {
+    providers: [
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                email: {label: "Email", type: "text"},
+                password: {label: "Password", type: "password"}
+            },
+            async authorize(credentials) {
+
+                const res = await fetch("https://your-backend.com/api/login", {
+                    method: 'POST',
+                    body: JSON.stringify(credentials),
+                    headers: {"Content-Type": "application/json"}
+                });
+
+                const user = await res.json();
+                
+
+            }
+        })
+    ]
+}
