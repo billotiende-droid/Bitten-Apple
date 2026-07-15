f4f48ebeabe45c35470488dba6f78255ef74f5b3
Onyango <allanbill34@gmail.com>
2026-07-15 15:08:37 +0300

    feat : install dotenv dependencies to load  set .env for postgress password

--- server/db.py
+++ server/db.py
@@ -2,8 +2,15 @@ import os
 from typing import AsyncGenerator
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from sqlalchemy.orm import DeclarativeBase
+from dotenv import load_dotenv
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+asyncpg://postgres:password@localhost:5423/gadgetstore_db")
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL environment is not set")
 
 class Base(DeclarativeBase):
     pass
